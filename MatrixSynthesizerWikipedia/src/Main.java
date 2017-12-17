2607ddcc39181ff2b847df47f166874e40895292
coren <corentin.despierre@yahoo.fr>
2017-12-17 19:34:52 +0100

    J'ai ajouté la partie CSV et la class gestionnaireCSV

--- MatrixSynthesizerWikipedia/src/Main.java
+++ MatrixSynthesizerWikipedia/src/Main.java
@@ -17,40 +17,33 @@ public class Main {
         String delimiter = ",";
         List<String> lObj = new ArrayList<String>();
 
-        try{
+        try {
             InputStream flux = new FileInputStream("MatrixSynthesizerWikipedia/File/FichierConfig.txt");
             InputStreamReader lecture = new InputStreamReader(flux);
             BufferedReader buff = new BufferedReader(lecture);
             String ligne;
-            while ((ligne=buff.readLine())!=null){
-                System.out.println(ligne);
+            while ((ligne = buff.readLine()) != null) {
                 temp = ligne.split(delimiter);
-                for(int i =0; i < temp.length ; i++){
+                for (int i = 0; i < temp.length; i++) {
                     int p1 = temp[i].indexOf('(');
                     int p2 = temp[i].indexOf(')');
-                    lObj.add(temp[i].substring(p1+1, p2));
+                    lObj.add(temp[i].substring(p1 + 1, p2));
                 }
             }
             buff.close();
-        }	catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
 
         //Récupération des données à partir des clés obtenues dans le fichier de configuration
-        String dataCsv ="";
-        for (String str : lObj){
-            //System.out.println(wikidataAPI.getEntity(str));
-            dataCsv += str +"\n";
+        String dataCsv = "";
+        for (String str : lObj) {
+            dataCsv += str + "\n";
         }
 
-        //System.out.println("GetPorperty : "+ wikidataAPI.getProperty("Q90"));
 
         //////********* PARTIE : WIKIPEDIA ********//////
         WikipediaAPI wikipediaAPI = new WikipediaAPI();
-
-        System.out.println("WIKIPEDIA !! ");
-        System.out.println(wikipediaAPI.searchEntity("Pomme_de_Terre_Lake"));
-        System.out.println(wikipediaAPI.getEntity("Pomme_de_Terre_Lake"));
 
         //Ajout des différentes recherches
         String name = "Lorient";
@@ -59,14 +52,14 @@ public class Main {
         String name4 = "Montpellier";
         String name5 = "Marseille";
 
-        Map<String,String> map1 =  wikipediaAPI.search(name,wikipediaAPI.getEntity(name));
-        Map<String,String> map2 =  wikipediaAPI.search(name2,wikipediaAPI.getEntity(name2));
-        Map<String,String> map3 =  wikipediaAPI.search(name3,wikipediaAPI.getEntity(name3));
-        Map<String,String> map4 =  wikipediaAPI.search(name4,wikipediaAPI.getEntity(name4));
-        Map<String,String> map5 =  wikipediaAPI.search(name5,wikipediaAPI.getEntity(name5));
-        Map<String,List<String>> mapFinal =  new LinkedHashMap<>();
+        Map<String, String> map1 = wikipediaAPI.search(name, wikipediaAPI.getEntity(name));
+        Map<String, String> map2 = wikipediaAPI.search(name2, wikipediaAPI.getEntity(name2));
+        Map<String, String> map3 = wikipediaAPI.search(name3, wikipediaAPI.getEntity(name3));
+        Map<String, String> map4 = wikipediaAPI.search(name4, wikipediaAPI.getEntity(name4));
+        Map<String, String> map5 = wikipediaAPI.search(name5, wikipediaAPI.getEntity(name5));
+        Map<String, List<String>> mapFinal = new LinkedHashMap<>();
 
-        List<Map<String,String>> list = new ArrayList<>();
+        List<Map<String, String>> list = new ArrayList<>();
         list.add(map1);
         list.add(map2);
         list.add(map3);
@@ -76,105 +69,71 @@ public class Main {
         int i = -1;
 
         //Résultat final
-        for(Map<String,String> map : list){
+        for (Map<String, String> map : list) {
             i++;
             Iterator it = map.keySet().iterator();
-            while (it.hasNext()){
+            while (it.hasNext()) {
                 List<String> listFinal;
 
                 Object cle = it.next();
                 Object valeur = map.get(cle);
 
-                cle = cle.toString().replace(" ","");
-               // System.out.println(cle + " " + cle.toString().length());
-                if(mapFinal.containsKey(cle)){
+                cle = cle.toString().replace(" ", "");
+                if (mapFinal.containsKey(cle)) {
                     listFinal = mapFinal.get(cle);
-                    //listFinal.add(valeur.toString());
-                    listFinal.add(i,valeur.toString());
-                    System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-                    System.out.println("Cle : " + cle);
-                    for(String str : listFinal){
-                        System.out.println("List : " + str);
-                    }
-                    System.out.println("taille : " + listFinal.size());
-                    if(listFinal.size()>list.size()){
-                        System.out.println("SSSSSSSSSSSSSSSSSSSSSiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
-                        for(int j=list.size();j<listFinal.size();j++){
+                    listFinal.add(i, valeur.toString());
+                    if (listFinal.size() > list.size()) {
+                        for (int j = list.size(); j < listFinal.size(); j++) {
                             listFinal.remove(j);
                         }
                     }
-                }else{
+                } else {
                     listFinal = new ArrayList<>();
                     if (i == 0) {
                         listFinal.add(valeur.toString());
-                        for(int j=1;j<list.size();j++){
-                            listFinal.add(j,"");
+                        for (int j = 1; j < list.size(); j++) {
+                            listFinal.add(j, "");
                         }
-                    }else{
-                        for(int j=0;j<i;j++){
-                            listFinal.add(j,"");
+                    } else {
+                        for (int j = 0; j < i; j++) {
+                            listFinal.add(j, "");
                         }
-                        listFinal.add(i,valeur.toString());
-                        for(int j=i+1;j<list.size();j++){
-                            listFinal.add(j,"");
+                        listFinal.add(i, valeur.toString());
+                        for (int j = i + 1; j < list.size(); j++) {
+                            listFinal.add(j, "");
                         }
-                        System.out.println("ListFinal : " + listFinal.size());
-                        System.out.println("List : " + list.size());
-
                     }
-                    System.out.println("**********************************************************************");
-                    System.out.println("Cle : " + cle);
-                    for(String str : listFinal){
-                        System.out.println("List : " + str);
-                    }
-                    mapFinal.put(cle.toString(),listFinal);
+                    mapFinal.put(cle.toString(), listFinal);
                 }
             }
         }
 
-        //OK
-
         String header = "";
         List<String> datas = new ArrayList<>();
-        for(int y=0;y<list.size();y++){
-            datas.add(y,"");
+        for (int y = 0; y < list.size(); y++) {
+            datas.add(y, "");
         }
 
         Iterator it = mapFinal.keySet().iterator();
-        while (it.hasNext()){
+        while (it.hasNext()) {
             //Colonnes
             Object cle = it.next();
-            cle = cle.toString().replace("\"","\'");
-            header += "\"" + cle + "\"" +",";
+            cle = cle.toString().replace("\"", "\'");
+            header += "\"" + cle + "\"" + ",";
 
 
             //Valeurs
             List<String> valeur = mapFinal.get(cle);
 
             String s = "";
-
-            System.out.println("Cle : " + cle);
-            System.out.println("Taille list : " + valeur.size());
-            System.out.println("Valeur : ");
-
-            for(int x=0; x<valeur.size();x++){
-                s = valeur.get(x).replace("\"","\'");
-                System.out.println(x +" : " +datas.get(x));
-
-                datas.set(x,datas.get(x)+ "\"" + s + "\""+",");
-                //System.out.println(datas.get(x));
+            for (int x = 0; x < valeur.size(); x++) {
+                s = valeur.get(x).replace("\"", "\'");
+                datas.set(x, datas.get(x) + "\"" + s + "\"" + ",");
             }
-            System.out.println(" ");
-            /*for(String s : valeur){
-                s = s.toString().replace("\"","\'");
-                data +=  "\"" + s + "\""+",";
-                System.out.print(s +" , ");
-            }
-            System.out.pr" Rome";intln();*/
         }
 
         String data = "";
-        for(String str : datas ){
+        for (String str : datas) {
             data += str + "\n";
         }
 
@@ -182,11 +141,11 @@ public class Main {
 
 
         GestionnaireCSV gestionnaire = new GestionnaireCSV();
-        try{
+        try {
             gestionnaire.addHeader("text", header, data);
             System.out.println("##################################################");
             System.out.println("Génération du csv '" + "text .csv' OK");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("\nErreur lors de la génération du CSV");
         }
 
--- MatrixSynthesizerWikipedia/src/WikipediaAPI.java
+++ MatrixSynthesizerWikipedia/src/WikipediaAPI.java
@@ -6,26 +6,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class WikipediaAPI implements WikiAPI {
-    @Override
-    public JSONObject searchEntity(String name)  throws IOException , JSONException {
-        String url = "https://en.wikipedia.org/w/api.php?action=query&list=prefixsearch&pssearch="+name+"&format=json";
 
+
+    @Override
+    public JSONObject searchEntity(String name) throws IOException, JSONException {
+        String url = "https://en.wikipedia.org/w/api.php?action=query&list=prefixsearch&pssearch=" + name + "&format=json";
         return search(url);
     }
 
     @Override
-    public JSONObject getEntity(String id)  throws IOException , JSONException {
-        String url = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles="+id+"&format=json";
+    public JSONObject getEntity(String id) throws IOException, JSONException {
+        String url = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=0&titles=" + id + "&format=json";
         return search(url);
     }
 
-    public JSONObject search(String str)  throws IOException , JSONException {
+
+
+    public JSONObject search(String str) throws IOException, JSONException {
         StringBuilder content = new StringBuilder();
 
         String url = str;
@@ -34,43 +35,33 @@ public class WikipediaAPI implements WikiAPI {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(searchConnection.getInputStream()));
 
         String line;
-        while ((line = bufferedReader.readLine()) != null)
-        {
-            //System.out.println(line);
+        while ((line = bufferedReader.readLine()) != null) {
             content.append(line + "\n");
-
         }
-        bufferedReader.close();
 
+        bufferedReader.close();
         return new JSONObject(content.toString());
     }
 
-    public Map<String,String> search(String name , JSONObject obj){
+    public Map<String, String> search(String name, JSONObject obj) {
         String str = obj.toString();
         int indexDébut = str.indexOf("box");
         int indexFin;
-        int indexMilieu = 0 ;
-        String scle,svaleur = "";
-        Map<String,String> map = new LinkedHashMap<>();
-        name = name.toString().replace("_"," ");
-        System.out.println("'''" + name + "'''");
-        System.out.println("Fin "+ str.indexOf("'''" + name));
+        int indexMilieu = 0;
+        String scle, svaleur = "";
+        Map<String, String> map = new LinkedHashMap<>();
+        name = name.toString().replace("_", " ");
 
-        while(indexDébut != -1 && indexDébut<str.indexOf("'''" + name) && indexMilieu<str.indexOf("'''" + name)) {
-            indexDébut = str.indexOf("|" , indexDébut);
-            indexMilieu = str.indexOf("=",indexDébut);
-            indexFin = str.indexOf("\\n",indexDébut);
-            System.out.println("*************************");
-            scle = str.substring(indexDébut+1,indexMilieu);
-            svaleur = str.substring(indexMilieu+1,indexFin);
-            System.out.println("clé : " + scle);
-            System.out.println("valeur : " + svaleur);
-            map.put(scle,svaleur);
-            indexDébut = indexFin ;
-            System.out.println("index : " + indexDébut);
-            indexDébut = str.indexOf("|" , indexDébut);
-            indexMilieu = str.indexOf("=",indexDébut);
-            System.out.println("index2 : " + indexDébut);
+        while (indexDébut != -1 && indexDébut < str.indexOf("'''" + name) && indexMilieu < str.indexOf("'''" + name)) {
+            indexDébut = str.indexOf("|", indexDébut);
+            indexMilieu = str.indexOf("=", indexDébut);
+            indexFin = str.indexOf("\\n", indexDébut);
+            scle = str.substring(indexDébut + 1, indexMilieu);
+            svaleur = str.substring(indexMilieu + 1, indexFin);
+            map.put(scle, svaleur);
+            indexDébut = indexFin;
+            indexDébut = str.indexOf("|", indexDébut);
+            indexMilieu = str.indexOf("=", indexDébut);
         }
 
         return map;
