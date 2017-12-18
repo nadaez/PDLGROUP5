e358852171d85ab8039409d0cccebc8fd9e7b70d
tgousset <t.gousset@hotmail.fr>
2017-12-18 22:27:08 +0100

    Ajout wikipedia

--- MatrixSynthesizerWikipedia/src/Main.java
+++ MatrixSynthesizerWikipedia/src/Main.java
@@ -6,65 +6,33 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException, JSONException {
+
+        System.out.println("Pour lancer une recherche vous devez saisir des informations dans le fichier : fichierConfig.txt");
+        System.out.println("Afin que les recherches puissent s'effectuer vous devez respecter la syntaxe de Wikipédia. ");
+        System.out.println("Exemple : si vous recherchez 'Pomme' il faudra marquer 'Apple' sinon il n'y aura aucune page correspondante. ");
+
+        /////////******* RECUPERATION DES DONNEES SUR LE FICHIER ********//////////
+        List<String> donneesWikipedia = donneesFichierWikiPedia();
+        //List<String> donneesWikidata = donneesFichierWikiData();
+
+        /////////******** PARTIE : WIKIDATA ********////////
         WikidataAPI wikidataAPI = new WikidataAPI();
+
         //Test des méthodes
-
-        System.out.println(wikidataAPI.searchEntity("Paris"));
-        System.out.println(wikidataAPI.getEntity("Q90"));
-
-        //Test avec un fichier de configuration
-        String[] temp;
-        String delimiter = ",";
-        List<String> lObj = new ArrayList<String>();
-
-        try {
-            InputStream flux = new FileInputStream("MatrixSynthesizerWikipedia/File/FichierConfig.txt");
-            InputStreamReader lecture = new InputStreamReader(flux);
-            BufferedReader buff = new BufferedReader(lecture);
-            String ligne;
-            while ((ligne = buff.readLine()) != null) {
-                temp = ligne.split(delimiter);
-                for (int i = 0; i < temp.length; i++) {
-                    int p1 = temp[i].indexOf('(');
-                    int p2 = temp[i].indexOf(')');
-                    lObj.add(temp[i].substring(p1 + 1, p2));
-                }
-            }
-            buff.close();
-        } catch (Exception e) {
-            System.out.println(e.toString());
-        }
-
-        //Récupération des données à partir des clés obtenues dans le fichier de configuration
-        String dataCsv = "";
-        for (String str : lObj) {
-            dataCsv += str + "\n";
-        }
-
+        //System.out.println(wikidataAPI.searchEntity("Paris"));
+        //System.out.println(wikidataAPI.getEntity("Q90"));
+        //System.out.println("GetPorperty : "+ wikidataAPI.getProperty("Q90"));
 
         //////********* PARTIE : WIKIPEDIA ********//////
         WikipediaAPI wikipediaAPI = new WikipediaAPI();
-
-        //Ajout des différentes recherches
-        String name = "Lorient";
-        String name2 = "Rennes";
-        String name3 = "Saint-Nazaire";
-        String name4 = "Montpellier";
-        String name5 = "Marseille";
-
-        Map<String, String> map1 = wikipediaAPI.search(name, wikipediaAPI.getEntity(name));
-        Map<String, String> map2 = wikipediaAPI.search(name2, wikipediaAPI.getEntity(name2));
-        Map<String, String> map3 = wikipediaAPI.search(name3, wikipediaAPI.getEntity(name3));
-        Map<String, String> map4 = wikipediaAPI.search(name4, wikipediaAPI.getEntity(name4));
-        Map<String, String> map5 = wikipediaAPI.search(name5, wikipediaAPI.getEntity(name5));
-        Map<String, List<String>> mapFinal = new LinkedHashMap<>();
-
         List<Map<String, String>> list = new ArrayList<>();
-        list.add(map1);
-        list.add(map2);
-        list.add(map3);
-        list.add(map4);
-        list.add(map5);
+        Map<String, List<String>> mapFinal = new LinkedHashMap<>();
+        GestionnaireCSV gestionnaire = new GestionnaireCSV();
+
+        for(String name : donneesWikipedia){
+            Map<String, String> map = wikipediaAPI.search(name, wikipediaAPI.getEntity(name));
+            list.add(map);
+        }
 
         int i = -1;
 
@@ -121,7 +89,6 @@ public class Main {
             cle = cle.toString().replace("\"", "\'");
             header += "\"" + cle + "\"" + ",";
 
-
             //Valeurs
             List<String> valeur = mapFinal.get(cle);
 
@@ -139,16 +106,69 @@ public class Main {
 
         header += "\n";
 
-
-        GestionnaireCSV gestionnaire = new GestionnaireCSV();
-        try {
-            gestionnaire.addHeader("text", header, data);
-            System.out.println("##################################################");
-            System.out.println("Génération du csv '" + "text .csv' OK");
-        } catch (Exception e) {
-            System.out.println("\nErreur lors de la génération du CSV");
+        if(data.length() != 0 ) {
+            try {
+                gestionnaire.addHeader("PCM", header, data);
+                System.out.println("##################################################");
+                System.out.println("Génération du csv '" + "PCM .csv' OK");
+            } catch (Exception e) {
+                System.out.println("\nErreur lors de la génération du CSV");
+            }
         }
 
 
+    }
+
+    public static List<String> donneesFichierWikiData(){
+        String[] temp;
+        String delimiter = ",";
+        List<String> lObj = new ArrayList<String>();
+
+        try{
+            InputStream flux = new FileInputStream("MatrixSynthesizerWikipedia/File/FichierConfig.txt");
+            InputStreamReader lecture = new InputStreamReader(flux);
+            BufferedReader buff = new BufferedReader(lecture);
+            String ligne;
+            while ((ligne=buff.readLine())!=null){
+                System.out.println(ligne);
+                temp = ligne.split(delimiter);
+                for(int i =0; i < temp.length ; i++){
+                    int p1 = temp[i].indexOf('(');
+                    int p2 = temp[i].indexOf(')');
+                    lObj.add(temp[i].substring(p1+1, p2));
+                }
+            }
+            buff.close();
+        }	catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        return lObj;
+    }
+
+    public static List<String> donneesFichierWikiPedia(){
+        String[] temp;
+        String delimiter = ",";
+        List<String> lObj = new ArrayList<String>();
+
+        try{
+            InputStream flux = new FileInputStream("MatrixSynthesizerWikipedia/File/FichierConfig.txt");
+            InputStreamReader lecture = new InputStreamReader(flux);
+            BufferedReader buff = new BufferedReader(lecture);
+            String ligne;
+            while ((ligne=buff.readLine())!=null){
+                System.out.println("Votre recherche : ");
+                temp = ligne.split(delimiter);
+                for(int i =0; i < temp.length ; i++){
+                    lObj.add(temp[i]);
+                    System.out.println(temp[i]);
+                }
+            }
+            buff.close();
+        }	catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        return lObj;
     }
 }
